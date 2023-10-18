906d2cd299913819f47a3aa1d84da75a96166f5d
ana27clara06 <4n4clara@gmail.com>
2023-10-18 11:33:15 -0300

    first commit

---
+++ anaclara.py
@@ -0,0 +1,7 @@
+km = int (input ("digite a velocidade em km:"))
+n1 = 60
+multa= km * 420
+if km <= n1 :
+    print ("limite de velocidade respeitada")
+else :
+    print (f"limite de velocidade respeitado, multa de {multa} ")
